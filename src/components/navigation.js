e4e26620f90cb496d17ff920e82ddd03cc03cf4d
aymane <76706091+aymanemhd@users.noreply.github.com>
2021-12-01 20:34:24 +0100

    add contact and services

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -41,23 +41,23 @@ function navigation() {
               </a>
             </li>
             <li>
-              <a href='#portfolio' className='page-scroll'>
-                Gallery
-              </a>
-            </li>
-            <li>
               <a href='#testimonials' className='page-scroll'>
                 Testimonials
               </a>
             </li>
             <li>
-              <a href='#team' className='page-scroll'>
-                Team
+              <a href='#contact' className='page-scroll'>
+                Contact
               </a>
             </li>
             <li>
-              <a href='#contact' className='page-scroll'>
-                Contact
+              <a href='#' className='login'>
+                Log In
+              </a>
+            </li>
+            <li>
+              <a href='#' className='login'>
+                Sing Up
               </a>
             </li>
           </ul>
